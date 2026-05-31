c421a83964af1fc1b3de37078a096adb0a1973a6
stecnologia <stecnologicas97@gmail.com>
2026-05-31 13:58:16 -0500

    se modifica el vite.config.ts para que suba el repo

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: process.env.VITE_BASE_PATH || "/res-ger-crm",
+    base: process.env.VITE_BASE_PATH || "/",
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
